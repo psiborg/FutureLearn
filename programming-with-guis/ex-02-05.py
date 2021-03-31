5237654b3d1f8d109e6f9f078c8c1377f17c645e
Jim Ing <jim.ing@gmail.com>
2021-03-31 02:43:36 -0400

    Ex. 2.5 and 2.6

---
+++ programming-with-guis/ex-02-05.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# programming-with-guis
+# Ex. 2.5
+
+# guizero - Hero name generator
+from guizero import App, Box, ButtonGroup, Combo, ListBox, PushButton, Slider, Text, TextBox
+app = App(title="Hero-o-matic", bg="#CCCCCC")
+
+# https://www.looper.com/101942/powerful-avengers-mcu-ranked/
+levels = {
+    "1": "Spider-Man",
+    "2": "Captain America",
+    "3": "Iron Man",
+    "4": "Black Panther",
+    "5": "Scarlet Witch",
+    "6": "The Vision",
+    "7": "Doctor Strange",
+    "8": "Thor",
+    "9": "The Hulk",
+    "10": "Captain Marvel"
+}
+
+bg_colours = {
+    "Black": "#000000", 
+    "White": "#FFFFFF", 
+    "Red": "#990000", 
+    "Yellow": "#999900", 
+    "Green": "#009900", 
+    "Cyan": "#009999", 
+    "Blue": "#000099", 
+    "Magenta": "#990099"
+}
+
+fg_colours = {
+    "Black": "#FFFFFF", 
+    "White": "#000000", 
+    "Red": "#FFFFFF", 
+    "Yellow": "#000000", 
+    "Green": "#FFFFFF", 
+    "Cyan": "#000000", 
+    "Blue": "#FFFFFF", 
+    "Magenta": "#000000"
+}
+
+# Function definitions for your events go here
+
+def on_resize(event):
+    #print(app.width, app.height)
+    w = app.width * 0.5
+    lst_colour.width = w
+
+def change_colour(val):
+    box_colour.bg = bg_colours[val]
+    box_colour.text_color = fg_colours[val]
+
+def slider_changed(slider_value):
+    txt_strength.value = levels[slider_value]
+
+def make_hero_name():
+    adjective = cmb_adjective.value
+    colour = lst_colour.value
+    animal = cmb_animal.value
+    hero = adjective + " " + colour + " " + animal
+    lbl_output.text_size = 16
+    lbl_output.text_color = bg_colours[lst_colour.value]
+    lbl_output.value = "The " + hero + " (Strength=" + levels[str(sld_strength.value)] + ")"
+
+# Your GUI widgets go here
+
+box_adjective = Box(app, border=0)
+lbl_adjective = Text(box_adjective, text="Adjective:")
+cmb_adjective = Combo(box_adjective, options=["Amazing", "Galactic", "Incredible", "Magnificent", "Mighty", "Valiant"], selected="Amazing")
+
+box_colour = Box(app, border=0)
+box_colour.bg = "#999999"
+lbl_colour = Text(box_colour, text="Colour:")
+w = 80
+h = 140
+lst_colour = ListBox(box_colour, items=["Black", "White", "Red", "Yellow", "Green", "Cyan", "Blue", "Magenta"], align="top", width=w, height=h, selected="Black", command=change_colour)
+
+box_animal = Box(app, border=0)
+lbl_animal = Text(box_animal, text="Animal:")
+cmb_animal = Combo(box_animal, options=["Bear", "Crow", "Deer", "Dolphin", "Eagle", "Hawk", "Horse", "Lion", "Owl", "Panda", "Panther", "Shark", "Snake", "Tiger", "Wolf"], selected="Panther", width=20)
+
+box_strength = Box(app, border=0)
+lbl_strength = Text(box_strength, text="Strength level:")
+sld_strength = Slider(box_strength, start="1", end="10", command=slider_changed)
+txt_strength = Text(box_strength)
+
+box_make_name = Box(app, border=0)
+btn_make_name = PushButton(box_make_name, text="Generate", command=make_hero_name)
+
+box_output = Box(app, border=1)
+#box_output.bg = "#999999"
+lbl_output = Text(box_output, text="Your hero name will appear here")
+
+# Set up event triggers here
+
+# Use tkinter event <Configure> on the canvas
+app.tk.bind('<Configure>', on_resize)
+
+# Show the GUI on the screen, start listening to events
+app.display()
---
+++ programming-with-guis/ex-02-06a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# programming-with-guis
+# Ex. 2.6a
+
+from guizero import App, Text
+
+# this time we have not set the layout to auto as it is default
+app = App(title="align")
+
+# create a series of widgets each with a different alignment
+top_text_1 = Text(app, text="top_text_1", align="top", bg="#330000", color="#FFFFFF")
+top_text_2 = Text(app, text="top_text_2", align="top", bg="#990000", color="#FFFFFF")
+top_text_3 = Text(app, text="top_text_3", align="top", bg="#FF0000", color="#FFFFFF")
+
+left_text_1 = Text(app, text="left_text_1", align="left", bg="#003300", color="#FFFFFF")
+left_text_2 = Text(app, text="left_text_2", align="left", bg="#009900", color="#FFFFFF")
+left_text_3 = Text(app, text="left_text_3", align="left", bg="#00FF00", color="#000000")
+
+right_text_1 = Text(app, text="right_text_1", align="right", bg="#000033", color="#FFFFFF")
+right_text_2 = Text(app, text="right_text_2", align="right", bg="#000099", color="#FFFFFF")
+right_text_3 = Text(app, text="right_text_3", align="right", bg="#0000FF", color="#FFFFFF")
+
+bottom_text_1 = Text(app, text="bottom_text_1", align="bottom", bg="#003333", color="#FFFFFF")
+bottom_text_2 = Text(app, text="bottom_text_2", align="bottom", bg="#009999", color="#FFFFFF")
+bottom_text_3 = Text(app, text="bottom_text_3", align="bottom", bg="#00FFFF", color="#000000")
+
+app.display()
